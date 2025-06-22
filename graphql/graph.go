e21fb4eaab8d31da97c7382d9c520c2a07f83d16
Raghavendra S I <raghavendras715@gmail.com>
2025-06-22 21:19:22 +0530

    updated graph.go

--- graphql/graph.go
+++ graphql/graph.go
@@ -1,50 +1,58 @@
-package main
+// package main
 
-type Server struct {
-	// 	accountClient *account.Client
-	// 	catalogClient *catalog.Client
-	// 	orderClient   *order.Client
-}
+// import "github.com/99designs/gqlgen/graphql"
 
-func NewGraphQLServer(accountUrl, catalogUrl, orderUrl string) (*Server, error) {
-	accountClient, err := account.NewClient(accountUrl)
-	if err != nil {
-		return nil, err
-	}
+// type Server struct {
+// 	// 	accountClient *account.Client
+// 	// 	catalogClient *catalog.Client
+// 	// 	orderClient   *order.Client
+// }
 
-	catalogClient, err := catalog.NewClient(catalogUrl)
-	if err != nil {
-		accountClient.Close()
-		return nil, err
-	}
+// func NewGraphQLServer(accountUrl, catalogUrl, orderUrl string) (*Server, error) {
+// 	accountClient, err := account.NewClient(accountUrl)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	orderClient, err := order.NewClient(orderUrl)
-	if err != nil {
-		accountClient.Close()
-		catalogClient.Close()
-		return nil, err
-	}
-	return &Server{
-		accountClient,
-		catalogClient,
-		orderClient,
-	}, nil
-}
+// 	catalogClient, err := catalog.NewClient(catalogUrl)
+// 	if err != nil {
+// 		accountClient.Close()
+// 		return nil, err
+// 	}
 
-func (s *Server) Mutation() MutationResolver {
-	return &mutationResolver{
-		server: s,
-	}
-}
+// 	orderClient, err := order.NewClient(orderUrl)
+// 	if err != nil {
+// 		accountClient.Close()
+// 		catalogClient.Close()
+// 		return nil, err
+// 	}
+// 	return &Server{
+// 		accountClient,
+// 		catalogClient,
+// 		orderClient,
+// 	}, nil
+// }
 
-func (s *Server) Query() QueryResolver {
-	return &queryResolver{
-		server: s,
-	}
-}
+// func (s *Server) Mutation() MutationResolver {
+// 	return &mutationResolver{
+// 		server: s,
+// 	}
+// }
 
-func (s *Server) Query() AccountResolver {
-	return &accountResolver{
-		server: s,
-	}
-}
+// func (s *Server) Query() QueryResolver {
+// 	return &queryResolver{
+// 		server: s,
+// 	}
+// }
+
+// func (s *Server) Account() AccountResolver {
+// 	return &accountResolver{
+// 		server: s,
+// 	}
+// }
+
+// func (s *Server) ToExecutableSchema() graphql.ExecutableSchema {
+// 	return NewExecutableSchema(Config{
+// 		Resolvers: s,
+// 	})
+// }
--- graphql/query_resolver.go
+++ graphql/query_resolver.go
@@ -1,5 +1,15 @@
 package main
 
+import "context"
+
 type queryResolver struct {
-	server *
+	server *Server
+}
+
+// func (r *queryResolver) Accounts(ctx context.Context, pagination *PaginationInput, id *string) ([]*Account, error) {
+
+// }
+
+func (r *queryResolver) Products(ctx context.Context, pagination *PaginationInput, query *string, id *string) ([]*Product, error) {
+
 }
