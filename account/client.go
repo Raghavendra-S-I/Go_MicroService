99eb2397078972faca43dcf113b01e2baed2b790
Raghavendra S I <raghavendras715@gmail.com>
2025-08-08 23:11:15 +0530

    Added  getaccount and getaccounts function methods for client.go file

--- account/client.go
+++ account/client.go
@@ -39,3 +39,38 @@ func (c *Client) PostAccount(ctx context.Context, name string) (*Account, error)
 		Name: r.Account.Name,
 	}, nil
 }
+
+func (c *Client) GetAccount(ctx context.Context, id string) (*Account, error) {
+	r, err := c.service.GetAccount(
+		ctx,
+		&pb.GetAccountRequest{Id: id},
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &Account{
+		ID:   r.Account.Id,
+		Name: r.Account.Name,
+	}, nil
+}
+
+func (c *Client) GetAccounts(ctx context.Context, skip uint64, take uint64) ([]Account, error) {
+	r, err := c.service.GetAccounts(
+		ctx,
+		&pb.GetAccountsRequest{
+			Skip: skip,
+			Take: take,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+	accounts := []Account{}
+	for _, a := range r.Accounts {
+		accounts = append(accounts, Account{
+			ID:   a.Id,
+			Name: a.Name,
+		})
+	}
+	return accounts, nil
+}
